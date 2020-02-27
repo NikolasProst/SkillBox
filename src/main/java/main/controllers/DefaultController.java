397dd7d33ebdc98ecdfd49409ef6f7a8aa1d6190
NikolasProst <ramanik32@gmail.com>
2020-02-27 16:41:57 +0300

    ready to merge

--- src/main/java/main/controllers/DefaultController.java
+++ src/main/java/main/controllers/DefaultController.java
@@ -1,10 +1,15 @@
 package main.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
+@Controller
 public class DefaultController {
-
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
 }
