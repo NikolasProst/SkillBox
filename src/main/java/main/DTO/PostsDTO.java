d7fc36b23e16ad61f0a4a00813fe1705a0c6f085
NikolasProst <ramanik32@gmail.com>
2020-04-02 21:28:56 +0300

    add JsonViews and post/{id} 0,15

--- src/main/java/main/DTO/PostsDTO.java
+++ src/main/java/main/DTO/PostsDTO.java
@@ -14,31 +14,31 @@ import java.util.List;
 @Data
 
 public class PostsDTO {
-    @JsonView({JsonViews.idPost.class, JsonViews.post.class})
+    @JsonView(JsonViews.post.class)
     private int id;
 
-    @JsonView({JsonViews.idPost.class, JsonViews.post.class})
+    @JsonView(JsonViews.post.class)
     private Date time;
 
-    @JsonView({JsonViews.idPost.class, JsonViews.post.class})
+    @JsonView(JsonViews.post.class)
     private PostAuthorDTO user;
 
-    @JsonView({JsonViews.idPost.class, JsonViews.post.class})
+    @JsonView(JsonViews.post.class)
     private String title;
 
-    @JsonView({JsonViews.idPost.class, JsonViews.post.class})
+    @JsonView(JsonViews.post.class)
     private String announce;
 
-    @JsonView({JsonViews.idPost.class, JsonViews.post.class})
+    @JsonView(JsonViews.post.class)
     private int likeCount;
 
-    @JsonView({JsonViews.idPost.class, JsonViews.post.class})
+    @JsonView(JsonViews.post.class)
     private int dislikeCount;
 
-    @JsonView({JsonViews.idPost.class, JsonViews.post.class})
+    @JsonView(JsonViews.post.class)
     private int commentCount;
 
-    @JsonView({JsonViews.idPost.class, JsonViews.post.class})
+    @JsonView(JsonViews.post.class)
     private int viewCount;
 
     @JsonView(JsonViews.idPost.class)
